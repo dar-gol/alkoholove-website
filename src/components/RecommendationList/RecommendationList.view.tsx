2b9890d035b88380d1061fa8a19bb07f3373105f
dar-gol <dariusz.w.golom@gmail.com>
2023-01-11 22:10:47 +0100

    fix recommendation list

--- src/components/RecommendationList/RecommendationList.view.tsx
+++ src/components/RecommendationList/RecommendationList.view.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import {Alcohols} from '../../@types/alcohol';
 import {ImageContainer, Img, Row} from '../../styles/global.styled';
-import { URL} from '../../utils/constant';
-import {createImageName} from '../../utils/utils';
+import {URL} from '../../utils/constant';
+import {createImageName, getRate} from '../../utils/utils';
 import Stars from '../Stars/Stars.view';
 import {
   AlcoholWrapper,
@@ -19,38 +19,42 @@ interface RecommendationListViewProps {
   alcohols: Alcohols;
 }
 
-export const RecommendationListView: React.FC<RecommendationListViewProps> = ({alcohols}) => {
+export const RecommendationListView: React.FC<RecommendationListViewProps> = ({
+  alcohols,
+}) => {
   const navigate = useNavigate();
   return (
     <RecommendationListContainer>
       {alcohols.map(alcohol => (
-          <AlcoholWrapper
-            key={alcohol.id}
-            onClick={() => navigate(`/alcohol/${alcohol.barcode[0]}`)}>
-            <ImageContainer zIndex={1}>
-              <Img
-                width="150px"
-                height="175px"
-                src={`${URL.GET_IMAGE}/${createImageName(
-                  alcohol.id.toLowerCase(),
-                  'md',
-                )}`}
+        <AlcoholWrapper
+          key={alcohol.id}
+          onClick={() => navigate(`/alcohol/${alcohol.barcode[0]}`)}>
+          <ImageContainer zIndex={1}>
+            <Img
+              width="150px"
+              height="175px"
+              src={`${URL.GET_IMAGE}/${createImageName(
+                alcohol.id.toLowerCase(),
+                'md',
+              )}`}
+            />
+          </ImageContainer>
+          <Content minWidth="300px" height="300px">
+            <Name>{alcohol.name}</Name>
+            <Type>
+              {alcohol.kind}, {alcohol.type}
+            </Type>
+            <DescTitle>Opis:</DescTitle>
+            <Description>{alcohol.description}</Description>
+            <Row flex="1" alignItems="center" justifyContent="center">
+              <Stars
+                rate={getRate(alcohol.rate_value, alcohol.rate_count)}
+                onlyStars
               />
-            </ImageContainer>
-            <Content minWidth="300px" height="300px">
-              <Name>{alcohol.name}</Name>
-              <Type>
-                {alcohol.kind}, {alcohol.type}
-              </Type>
-              <DescTitle>Opis:</DescTitle>
-              <Description>{alcohol.description}</Description>
-              <Row flex="1" alignItems="center" justifyContent="center">
-                <Stars rate={5} />
-              </Row>
-            </Content>
-          </AlcoholWrapper>
-        )
-      )}
+            </Row>
+          </Content>
+        </AlcoholWrapper>
+      ))}
     </RecommendationListContainer>
   );
 };
