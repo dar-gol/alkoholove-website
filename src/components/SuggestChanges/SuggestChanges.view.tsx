36ab686985dc4ee217d019146ec47e01a4f5bb67
Mateusz Romański <mateusz_romanski@interia.pl>
2023-01-18 22:25:47 +0000

    fix: require description for suggestion [ALKL-519]

--- src/components/SuggestChanges/SuggestChanges.view.tsx
+++ src/components/SuggestChanges/SuggestChanges.view.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
-import {useTheme} from 'styled-components';
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import styled, {useTheme} from 'styled-components';
 import {IAlcohol} from '../../@types/alcohol';
-import {BtnPrimary, Col, Row, Text} from '../../styles/global.styled';
+import {BtnPrimary, Col, Form, Row, Text} from '../../styles/global.styled';
 import TextInput from '../Inputs/TextInput';
 import Modal from '../modal/Modal';
 
@@ -12,12 +13,25 @@ interface Props {
   sendError: (description: string) => void;
 }
 
+type SuggestChange = {
+  description: string
+}
+
+const defaultValues = {
+  description: "",
+};
+
+const StyledForm = styled(Form)`
+  min-width: 300px;
+  gap: 10px;
+  padding: 0;
+` 
+
 const SuggestChanges = ({alcohol, isOpen, onClose, sendError}: Props) => {
   const theme = useTheme();
-  const [description, setDescription] = useState('');
-  const handleClick = () => {
-    sendError(description);
-    setDescription('');
+  const form = useForm({ defaultValues });
+  const onSubmit = (data: SuggestChange) => {
+    sendError(data.description);
     onClose();
   };
   return (
@@ -42,21 +56,31 @@ const SuggestChanges = ({alcohol, isOpen, onClose, sendError}: Props) => {
             </Text>
           )}
         </Col>
-        <Row minWidth="300px">
-          <TextInput
-            value={description}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setDescription(e.target.value)
-            }
-            icon="icon-Flagged_comment"
-            type="textarea"
-            title="Opisz problem"
-            placeholder="Niepoprawny typ"
-            state=""
-            error=""
+        <StyledForm onSubmit={form.handleSubmit(onSubmit)}>
+        <Row width="100%">
+          <Controller
+            control={form.control}
+            name="description"
+            rules={{
+              required: true,
+            }}
+            render={({field}) => (
+              <TextInput
+                value={field.value}
+                onChange={field.onChange}
+                inputRef={field.ref}
+                icon="icon-Flagged_comment"
+                type="textarea"
+                title="Opisz problem"
+                error="Opis problemu jest wymagany"
+                placeholder="Niepoprawny typ"
+                state={form.formState.errors.description ? 'error' : ''}
+              />
+            )}
           />
-        </Row>
-        <BtnPrimary onClick={() => handleClick()}>Zaproponuj zmiany</BtnPrimary>
+          </Row>
+          <BtnPrimary type="submit" width='100%'>Zaproponuj zmiany</BtnPrimary>
+        </StyledForm>
       </Col>
     </Modal>
   );
